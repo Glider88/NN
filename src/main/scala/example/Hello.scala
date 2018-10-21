1ff4002792d8f7992b2aef4e7f63fe05036d1525
Pavel <mail>
2018-10-21 20:57:22 +0300

    progress

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -24,13 +24,26 @@ object Hello extends App {
 //  L0     L1     L2
 //         ◯
 //  ◯      ◯
-//  ◯  S1  ◯  S2  ◯
+//  ◯  S0  ◯  S1  ◯
 //  ◯      ◯
+//
+//  X             y
+//
+// L2_error = y - L2
+// L2_norm  = L2_error * sigmoid'(L2)
+//
+// L1_error = L2_norm * S1
+// L1_norm  = L1_error * sigmoid'(L1)
+//
+// S1 = S1 + L1 * L2_norm
+// S0 = S0 + L0 * L1_norm
 
   // val E = 60000
   // val R = 4
   var S: MatrixList = List(3, 4, 1).init.zip(List(3, 4, 1).tail).map(l => createRandomMatrix(l._1, l._2))
   var L: MatrixList = List()
+  var D: MatrixList = List()
+
 
   var syn0 = createRandomMatrix(3, 4)
   var syn1 = createRandomMatrix(4, 1)
@@ -41,19 +54,18 @@ object Hello extends App {
     L = S.foldLeft(List(X))((l: MatrixList, s: Matrix) => l :+ sigmoid(l.last * s))
 
 
-    def delta(prev: Matrix, next: Matrix): Matrix = {
-      val error = next - prev
-      error :* sigmoidDerivation(prev)
-    }
-
-    val l0 = X
-    val l1 = sigmoid(l0 * syn0)
-    val l2 = sigmoid(l1 * syn1)
 
 
     // if (i % 10000 == 0) {
     //   println("Error:" + mean(abs(l2_error)))
     // }
+
+
+
+    val l0 = X
+    val l1 = sigmoid(l0 * syn0)
+    val l2 = sigmoid(l1 * syn1)
+
 
     val l2_error = y - l2
     val l2_delta = l2_error :* sigmoidDerivation(l2)
@@ -63,6 +75,8 @@ object Hello extends App {
 
     syn1 += l1.t * l2_delta
     syn0 += l0.t * l1_delta
+
+
 
     // i += 1
   // }
